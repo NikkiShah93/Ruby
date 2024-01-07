207bfd07e9afc2a8fa0dd9d9bdf427c4afb1074b
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-01-07 18:14:27 -0500

    constants and files

--- basic/main.rb
+++ basic/main.rb
@@ -22,3 +22,26 @@ puts "6 - 4 = "+ (6-4).to_s
 puts "6 * 4 = "+ (6*4).to_s
 puts "6 / 4 = "+ (6/4).to_s
 puts "6 % 4 = "+ (6%4).to_s
+## with floats
+## 14 digits of accuracy is norm
+big_float = 1.12345678901234
+puts (big_float + 0.0000000000005).to_s
+## everything is a class in ruby
+puts 1.class
+puts 1.2.class 
+puts "this is a string".class
+## a constant starts with an uppercase letter
+A_CONSTANT = 4.67
+A_CONSTANT = 1.6 
+## it'll allow you to change it
+## but will throw a warning
+puts A_CONSTANT
+## if you want to read and write to files
+## if we wanted to create a new file
+## we can do the follwoing
+write_handler = File.new("newFile.out", "w")
+write_handler.puts("This is the content").to_s
+write_handler.close
+## and then for reading
+data_from_file = File.read("newFile.out")
+puts "File content: "+data_from_file
