27e7564819bcbe3653ba19208e9dc2172653e4ed
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-01-07 17:50:50 -0500

    initial commit

---
+++ basic/main.rb
@@ -0,0 +1,14 @@
+## Ruby basics
+## 
+print "Hello World!"
+## variable starts with lowercase
+## or _
+## for getting data from user
+## use gets
+## and convert it to other types
+## the following way
+print "Enter a number: "
+first_num = gets.to_i
+print "Enter another number: "
+second_num = gets.to_i 
+
