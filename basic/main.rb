32bfd60d78ccc1858d8be3e7d6b8de08e54057f4
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-01-07 17:59:46 -0500

    arithmetic operators

--- basic/main.rb
+++ basic/main.rb
@@ -1,5 +1,5 @@
 ## Ruby basics
-## 
+## everything is an object
 print "Hello World!"
 ## variable starts with lowercase
 ## or _
@@ -11,4 +11,14 @@ print "Enter a number: "
 first_num = gets.to_i
 print "Enter another number: "
 second_num = gets.to_i 
-
+## and output data
+## put adds a new line
+## where print doesn't
+puts first_num.to_s + " + " +  second_num.to_s + " = " + (first_num + second_num).to_s
+## there are 5 arithmetic operators
+## +, -, *, /, %
+puts "6 + 4 = "+ (6+4).to_s
+puts "6 - 4 = "+ (6-4).to_s
+puts "6 * 4 = "+ (6*4).to_s
+puts "6 / 4 = "+ (6/4).to_s
+puts "6 % 4 = "+ (6%4).to_s
